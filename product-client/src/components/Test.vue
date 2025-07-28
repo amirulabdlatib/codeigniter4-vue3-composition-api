ec7ee39dcbc5312181a57e3b73b0264c0fcc99a8
Amirul <amirulal73@gmail.com>
2025-07-28 16:29:46 +0800

    Test the vue'

---
+++ product-client/src/components/Test.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h2>This is test component</h2>
+  </div>
+</template>
---
+++ product-client/src/views/TestView.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import Test from "@/components/Test.vue";
+</script>
+
+<template>
+    <div>
+        <h1>Test Page</h1>
+        <Test />
+    </div>
+</template>
