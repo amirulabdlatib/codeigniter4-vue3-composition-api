24068b0f230bcaeb43196b827cc3b8ff39162050
Amirul <amirulal73@gmail.com>
2025-07-28 18:21:32 +0800

    Add create product table

---
+++ product-api/app/Database/Migrations/2025-07-28-101238_CreatePoductTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePoductTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'auto_increment' => true,
+                'null' => false,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 256,
+                'null' => false,
+
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => false,
+            ],
+            'price' => [
+                'type' => 'DECIMAL',
+                'constraint' => "10.2",
+                'null' => false
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('products');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('products');
+    }
+}
