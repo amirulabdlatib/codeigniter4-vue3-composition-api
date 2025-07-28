1db8977a5185be8c6e380929fe66ad49c5d2aff8
Amirul <amirulal73@gmail.com>
2025-07-28 17:32:28 +0800

    install tailwind css

--- product-client/src/App.vue
+++ product-client/src/App.vue
@@ -5,9 +5,9 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/test">Test</RouterLink>
+      <nav class="text-blue-500 underline space-x-3 ">
+        <RouterLink to="/" class="hover:text-blue-800">Home</RouterLink>
+        <RouterLink to="/test" class="hover:text-blue-800">Test</RouterLink>
       </nav>
     </div>
   </header>
