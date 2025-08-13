3420315eb25cddf7ea04c5302601c752ef4cecdf
amirulabdlatib <amirulal73@gmail.com>
2025-08-13 17:46:04 +0800

    Remove meta title

--- product-client/src/router/index.js
+++ product-client/src/router/index.js
@@ -1,34 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import TestView from '@/views/TestView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import TestView from "@/views/TestView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
-      meta:{
-        title:'Home'
-      }
     },
-
     {
-      path: '/test',
-      name: 'test',
+      path: "/test",
+      name: "test",
       component: TestView,
-      meta:{
-        title:'Test'
-      }
     },
   ],
-})
+});
 
-router.beforeEach((to, from, next) => {
-    const defaultTitle = 'My App'
-    document.title = to.meta.title || defaultTitle
-    next()
-})
-
-export default router
+export default router;
